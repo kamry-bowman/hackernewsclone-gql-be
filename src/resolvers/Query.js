272d2c2a95deb5d7423ef0403bcd4073a3a0b8aa
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-27 16:49:53 -0700

    implement pagination, sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,22 @@
-const feed = (root, args, context, info) => {
-  return context.prisma.links();
+const feed = async (root, args, context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+  return links;
 };
 
 module.exports = {
-  feed
+  feed,
 };
