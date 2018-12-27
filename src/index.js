52210d74d0aeb774fdea22b471f6304c6bf9e1d9
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-27 12:19:57 -0700

    Replace in-memory storage with prisma db

--- src/index.js
+++ src/index.js
@@ -1,57 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-];
-
-let idCount = links.length;
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links,
-    link: (parent, args) => {
-      return links.filter(link => link.id === args.id)[0];
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
     }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent, args) => {
-      links = links.map(link => {
-        if (link.id === args.id) {
-          return { ...link, ...args };
-        } else {
-          return link;
-        }
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
       });
-      return links.filter(link => link.id === args.id)[0];
-    },
-    deleteLink: (parent, args) => {
-      for (let i = 0; i < links.length; i++) {
-        const link = links[i];
-        if (args.id === link.id) {
-          links.splice(i, 1);
-          return link;
-        }
-      }
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log('Server is running on 4000'));
