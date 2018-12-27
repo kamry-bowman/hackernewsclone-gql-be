0f085e157e8495f152675cd3879eb83f5a510130
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-26 23:58:20 -0700

    implement crud for links

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 let links = [
   {
     id: 'link-0',
@@ -21,20 +8,50 @@ let links = [
   }
 ];
 
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links
+    feed: () => links,
+    link: (parent, args) => {
+      return links.filter(link => link.id === args.id)[0];
+    }
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      links = links.map(link => {
+        if (link.id === args.id) {
+          return { ...link, ...args };
+        } else {
+          return link;
+        }
+      });
+      return links.filter(link => link.id === args.id)[0];
+    },
+    deleteLink: (parent, args) => {
+      for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        if (args.id === link.id) {
+          links.splice(i, 1);
+          return link;
+        }
+      }
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './schema.graphql',
   resolvers
 });
 server.start(() => console.log('Server is running on 4000'));
