88fe1724917ef5d75b28490ace1e47730d1bffa8
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-27 18:01:21 -0700

    add sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -8,13 +8,23 @@ const feed = async (root, args, context, info) => {
       }
     : {};
 
-  const links = await context.prisma.links({
+  const linksPromise = context.prisma.links({
     where,
     skip: args.skip,
     first: args.first,
     orderBy: args.orderBy,
   });
-  return links;
+
+  const countPromise = context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count();
+
+  const [links, count] = await Promise.all([linksPromise, countPromise]);
+
+  return { links, count };
 };
 
 module.exports = {
